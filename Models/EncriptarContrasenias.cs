5175e9b8841c5ab58197d36a82757d2ddcc2aa7f
leonardo lobo <loboleonardo75@gmail.com>
2024-11-27 20:52:38 -0400

    Guardando datos

---
+++ Models/EncriptarContrasenias.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+namespace Proyecto_Blog.Models
+{
+    public class EncriptarContrasenia
+    {
+
+        public static string HashPassword(string password)
+        {
+            // Generar un salt único
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] saltBytes = new byte[16];
+                rng.GetBytes(saltBytes);
+                string salt = Convert.ToBase64String(saltBytes);
+
+                // Crear el hash de la contraseña con el salt
+                using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000))
+                {
+                    byte[] hashBytes = pbkdf2.GetBytes(32);
+                    string hashedPassword = Convert.ToBase64String(hashBytes);
+
+                    return hashedPassword;
+                }
+            }
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Proyecto_Blog.Models
+{
+    public class Usuario
+    {
+        private int id { get; set; }
+        private string nombreUsuario { get; set; } = "";
+        private string correo { get; set; } = "";
+        private string contrasenia { get; set; } = "";
+
+        public Usuario(string nombreUsuario, string correo, string contrasenia)
+        {
+            this.nombreUsuario = nombreUsuario;
+            this.correo = correo;
+            this.contrasenia = contrasenia;
+        }
+        public string getNombreUsuario()
+        {
+            return nombreUsuario;
+        }
+        public string getCorreo()
+        {
+            return correo;
+        }
+        public string getContraseniaEncriptada()
+        {
+            return EncriptarContrasenia.HashPassword(contrasenia);
+        }
+    }
+}
