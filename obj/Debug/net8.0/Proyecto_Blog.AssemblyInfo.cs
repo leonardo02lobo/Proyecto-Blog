fa99b4a706e4516841a8a1060a3929abd7bca06a
leonardo lobo <loboleonardo75@gmail.com>
2024-12-29 19:36:13 -0400

    guardando datos

--- obj/Debug/net8.0/Proyecto_Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto_Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd3d8f716726dd499f5ce190f0f6dc8988208de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e06b3ea359a1ab81865f133ef74b005b99005b88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
